a91e920d07f760a74dc8ae7c438d71620407fee0
AdityaNambiar <aditya.nambiar24@gmail.com>
2025-12-20 23:02:45 +0530

    react_learn: Lesson 2 (2a) done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import './App.css';
+// eslint-disable-next-line
 import ButtonExercise from './components/lesson1/ButtonExercise';
+// eslint-disable-next-line
 import ParaExercise from './components/lesson1/ParaExercise';
+// eslint-disable-next-line
 import SampleDesign from './components/lesson1/SampleDesign';
+// eslint-disable-next-line
 import JSSumOneD from './components/lesson1/JSSumOneD';
+// eslint-disable-next-line
 import DisplayDateOneG from './components/lesson1/DisplayDateOneG';
+// eslint-disable-next-line
 import DisplayDateInParaOneH from './components/lesson1/DisplayDateInParaOneH';
+// eslint-disable-next-line
 import DisplayTimeOneI from './components/lesson1/DisplayTimeOneI';
+
+import SampleText2A from './components/lesson2/SampleText2A';
 /**
  * Exercises from Lesson 1
  */
+// function App() {
+//   return (
+//     <>
+//       <ButtonExercise /><hr/>
+//       <ParaExercise /><hr/>
+//       <SampleDesign /><hr/>
+//       <JSSumOneD /><hr/>
+//       <DisplayDateOneG /><hr/>
+//       <DisplayDateInParaOneH /> <hr/>
+//       <DisplayTimeOneI /><hr/>
+//     </>
+//   );
+// }
+
 function App() {
   return (
     <>
-      <ButtonExercise /><hr/>
-      <ParaExercise /><hr/>
-      <SampleDesign /><hr/>
-      <JSSumOneD /><hr/>
-      <DisplayDateOneG /><hr/>
-      <DisplayDateInParaOneH /> <hr/>
-      <DisplayTimeOneI /><hr/>
+      <SampleText2A /><hr/>
     </>
   );
 }
-
 export default App;
---
+++ src/components/lesson2/SampleText2A.jsx
@@ -0,0 +1,11 @@
+import { Component }   from "react";
+class SampleText2A extends Component {
+    render() { 
+        return (
+            <div>
+                Hello, welcome to my website!
+            </div>
+        );
+    }
+}
+export default  SampleText2A;
