68a7557a5c9a5cbf96007a50d4c5f2a0e2eb011e
AdityaNambiar <aditya.nambiar24@gmail.com>
2025-12-20 21:05:11 +0530

    react_learn: few corrections + lesson_one exercise(1d)

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import './App.css';
 import ButtonExercise from './components/lesson1/ButtonExercise';
 import ParaExercise from './components/lesson1/ParaExercise';
 import SampleDesign from './components/lesson1/SampleDesign';
+import JSSumOneD from './components/lesson1/JSSumOneD';
+/**
+ * Exercises from Lesson 1
+ */
 function App() {
   return (
     <>
-      <ButtonExercise />
-      <ParaExercise />
-      <SampleDesign />
+      <ButtonExercise /><hr/>
+      <ParaExercise /><hr/>
+      <SampleDesign /><hr/>
+      <JSSumOneD /><hr/>
     </>
   );
 }
--- src/components/lesson1/SampleDesign.jsx
+++ src/components/lesson1/SampleDesign.jsx
@@ -1,15 +1,14 @@
 import { Component }   from "react";
 import ButtonExercise from "./ButtonExercise";
-class ParaExercise extends Component {
+class SampleDesign extends Component {
     render() { 
         return (
             <div> 
-                <hr/>
-                <span>Cotton Socks</span>
+                <span>Cotton Socks</span><br/>
                 <span>Price: $5</span>
                 <ButtonExercise value="Add to Cart"/>
             </div>
         );
     }
 }
-export default ParaExercise;
+export default SampleDesign;
